86db372046cec1559de57ed4ad29a13a915dd14c
Moniqchege <quichege33@gmail.com>
2024-04-19 17:18:49 +0300

    update

--- responsive-modern-ui-ux-website/src/components/Button.jsx
+++ responsive-modern-ui-ux-website/src/components/Button.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
-  return (
-    <div>
-      Button
-    </div>
-  )
-}
+const Button = ({ styles }) => (
+  <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none transition duration-300 ease-in-out
+  hover:bg-blue-600 hover:scale-105 hover:shadow-lg ${styles}`}>
+    Get Started
+  </button>
+);
 
-export default Button
+export default Button;
